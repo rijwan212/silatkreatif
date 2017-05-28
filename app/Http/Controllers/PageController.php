4ffbd35d63de61b0cd1709d649642e4e5152cd1e
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-05-28 19:27:52 +0700

    Update page controller

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,5 +6,39 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+
+    public function getBeranda()
+    {
+        return view('beranda');
+    }
+
+    public function getSilatBetawi()
+    {
+        return view('silatbetawi');
+    }
+
+    public function getSeminar()
+    {
+        return view('seminar');
+    }
+
+    public function getWorkshop()
+    {
+        return view('workshop');
+    }
+
+    public function getGaleriFoto()
+    {
+        return view('foto');
+    }
+
+    public function getGaleriVideo()
+    {
+        return view('video');
+    }
+
+    public function getDaftarSilat()
+    {
+        return view('indexx');
+    }
 }
