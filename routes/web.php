aabab9b17f371fe8320c1ef8abbe1f03b3cf45c4
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-05-28 19:28:07 +0700

    Fix route page

--- routes/web.php
+++ routes/web.php
@@ -12,35 +12,14 @@
 
 */
 
-Route::get('/','FrontController@getIndex');
-Route::get('silatbetawi', 'FrontController@getSB');
-Route::get('seminar','FrontController@getSM');
-Route::get('workshop', 'FrontController@getWS');
-Route::get('foto','FrontController@getFT');
-Route::get('video', 'FrontController@getVD');
-Route::get('unjuk','FrontController@getUK');
-Route::get('festival', 'FrontController@getFS');
-Route::get('detail/{id}', 'FrontController@getDetail');
-Route::get('situs/{id}', 'FrontController@getST');
-Route::get('situsseminar/{id}', 'FrontController@getSS');
-
-Route::get('form','FrontController@getFM');
-
-/*Route::post('/mhs', 'FrontController@store')->name('store');
-Route::get('/mhs', 'FrontController@index')->name('index');
-Route::get('/mhs/create', 'FrontController@create')->name('create');
-Route::delete('/mhs/{id}', 'FrontController@destroy')->name('destroy');
-Route::put('/mhs/{id}', 'FrontController@update')->name('update');
-Route::get('/mhs/{id}', 'FrontController@show')->name('show');
-Route::get('/mhs/{id}/edit', 'FrontController@edit')->name('edit');*/
-
-Route::post('/dft', 'HomeController@store')->name('store');
-Route::get('/dftr', 'HomeController@index')->name('index');
-Route::get('/dftr/create', 'HomeController@create')->name('create');
-Route::delete('/dftr/{id}', 'HomeController@destroy')->name('destroy');
-Route::put('/dftr/{id}', 'HomeController@update')->name('update');
-Route::get('/dftr/{id}', 'HomeController@show')->name('show');
-Route::get('/dftr/{id}/edit', 'HomeController@edit')->name('edit');
+// Front Page
+Route::get('/','PageController@getBeranda')->name('page.beranda');
+Route::get('silat-betawi', 'PageController@getSilatBetawi')->name('page.silat-betawi');
+Route::get('seminar', 'PageController@getSeminar')->name('page.seminar');
+Route::get('workshop', 'PageController@getWorkshop')->name('page.workshop');
+Route::get('galeri/foto', 'PageController@getGaleriFoto')->name('page.foto');
+Route::get('galeri/video', 'PageController@getGaleriVideo')->name('page.video');
+Route::get('daftar-silat', 'PageController@getDaftarSilat')->name('page.daftar-silat');
 
 //Auth::routes();
 
